0dbcd465f7d8130748a1b6863de1eb66bcaa5387
dunnwithpuns <dunnwithpuns@gmail.com>
2024-10-30 14:50:17 -0500

    idk wtf I'm doing

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Pokedex > ")
+
+	for scanner.Scan() {
+		fmt.Println("Pokedex > ")
+	}
+
+	if scanner.Err() != nil {
+		fmt.Println(scanner.Err())
+	}
+}
+
+func initCommands() {
+	type cliCommand struct {
+		name        string
+		description string
+		callback    func() error
+	}
+
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exits the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
