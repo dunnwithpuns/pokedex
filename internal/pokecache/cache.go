24e265602417455c29d939de4b818aebedaa5e1c
dunnwithpuns <dunnwithpuns@gmail.com>
2024-11-04 16:20:55 -0600

    created cache.Get and cache.Add not sure how to make cache.reaploop or newcache just yet

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -1,14 +1,51 @@
 package pokecache
 
 import (
+	"sync"
 	"time"
 )
 
 type Cache struct {
+	Data map[string]cacheEntry
+	mu   *sync.RWMutex
+}
+
+type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
 }
 
 func NewCache(interval time.Duration) Cache {
+
 	return Cache
+}
+
+func (cache *Cache) Add(key string, val []byte) {
+
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	newEntry := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+
+	cache.Data[key] = newEntry
+}
+
+func (cache *Cache) Get(key string) ([]byte, bool) {
+
+	cache.mu.RLock()
+	defer cache.mu.RUnlock()
+
+	requestedData, ok := cache.Data[key]
+	if !ok {
+		return []byte{}, false
+	}
+
+	return requestedData.val, true
+}
+
+func (cache *Cache) reaploop() {
+
 }
