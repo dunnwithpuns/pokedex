4cae712ae334edc6802ba1a28a58e3ceb34f8137
dunnwithpuns <dunnwithpuns@gmail.com>
2024-11-05 17:52:34 -0600

    trying to do it without watching the video, failing, but that should lead to success later right?

--- command_explore.go
+++ command_explore.go
@@ -5,5 +5,16 @@ import (
 )
 
 func commandExplore(cfg *config, commandParameter string) error {
+	response, err := cfg.pokeapiClient.ExploreArea(commandParameter)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Pokemon in the area:")
+
+	for _, pokemon := range response.PokemonEncounters {
+		fmt.Printf("- %v", pokemon)
+	}
+
 	return nil
 }
